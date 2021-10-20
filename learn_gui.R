b74dc4f2bca8f5790743931a6efbf25ca876e85e
89605502155 <ferubko1999@yandex.ru>
2021-10-21 02:08:23 +0300

    I continue to learn gwidget2

--- learn_gui.R
+++ learn_gui.R
@@ -1,6 +1,8 @@
 library(gWidgets)
 install.packages('gWidgets2tcltk')
 install.packages('gWidgets2')
+library(gWidgets2)
+
 library(gWidgets2tcltk)
 install.packages('lubridate')
 
@@ -16,3 +18,13 @@ my_win <- gbasicdialog("My Window",visible  = T, do.buttons=FALSE)
 gbutton("Cancel", container = my_win,handler = function(h,...) {
   my_win$dispose_window()
 })
+
+#gbutton("Hello world", container = gwindow())
+glabel("albatross", container = my_win)
+gedit("name", container = my_win)
+gradio(c("Go","Python","R"), container=my_win)
+gcombobox(c("Go","Python","R"), container=my_win)
+gcheckboxgroup(c("Perl","SQL"), container=my_win)
+gcalendar(text = as.character(Sys.Date()),container=my_win)
+gbutton("Select file", container=my_win, handler =
+function(h,...) {gfile(text = "Select file", type = "selectdir")})
